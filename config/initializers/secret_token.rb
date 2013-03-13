df0e59df433fef616d93c1a5022005963a566391
Shogo Kawaguchi <platycod0n.sj2826@gmail.com>
2013-03-13 22:44:01 +0900

    add rails init files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Picteract::Application.config.secret_token = '488714d4832d5838c04d611562a2bdf69f2800a9cc0d3769f29a52d025bb17eb3b60d367b694b08d240de977be5e217145e6d594d259b2b6fc693ef9c1f1992b'
